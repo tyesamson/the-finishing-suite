c9be83965c29439dd07fb6e147a2e65e9e447969
Matt Dwen <matt@dwen.co.nz>
2018-02-07 13:29:54 +1300

    Fix nav
    Fixes #10

--- src/js/jquery.scroll.js
+++ src/js/jquery.scroll.js
@@ -1,7 +1,48 @@
 
-$(document).ready(function($) {
-	$(".scroll").click(function(event){		
-		event.preventDefault();
-		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-	});
+$(document).ready(function ($) {
+  // Cache selectors
+  var lastId,
+    topMenu = $("#menu"),
+    topMenuHeight = topMenu.outerHeight() + 15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function () {
+      var href = $(this).attr('href');
+      if (href.substring(0,1) !== '#') {
+        return null;
+      }
+      var item = $($(this).attr("href"));
+      if (href.substring(0,1) === '#' && item.length) { return item; }
+    });
+
+  $(".scroll").click(function (event) {
+    event.preventDefault();
+    $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 500);
+  });
+
+  // Bind to scroll
+  $(window).scroll(function () {
+    // Get container scroll position
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+
+    // Get id of current scroll item
+    var cur = scrollItems.map(function () {
+      if ($(this).offset().top < fromTop)
+        return this;
+    });
+
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+      lastId = id;
+
+      // Set/remove active class
+      $('.active', '#menu').removeClass('active');
+      $(`[href="#${id}"]`, '#menu').addClass('active');
+      return;
+    }
+  });
 });
