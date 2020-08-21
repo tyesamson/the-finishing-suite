6d505c3c56ac3ffde84440b0c2a6f310ee14b6df
Matt Dwen <matt@dwen.co.nz>
2020-08-21 18:51:03 +1200

    Pre load portfolio images

--- resources/js/modules/lazy-load/index.js
+++ resources/js/modules/lazy-load/index.js
@@ -3,31 +3,45 @@ import { exists } from '@utilities/helpers'
 
 const LazyLoad = function LazyLoad()
 {
-  const images = $$.body.querySelectorAll('[data-lazy]');
-  if (!exists(images)) { return; }
+  function lazyLoad() {
+    const images = $$.body.querySelectorAll('[data-lazy]');
+    if (!exists(images)) { return; }
 
-  const options = {
-    threshold: 0.5
-  };
+    const options = {
+      threshold: 0.5
+    };
 
-  const preloadImage = function preloadImage(img) {
-    const src = img.dataset.lazy;
-    img.src = src;
-    img.classList.add('loaded');
-  };
+    const preloadImage = function preloadImage(img) {
+      const src = img.dataset.lazy;
+      img.src = src;
+      img.classList.add('loaded');
+    };
 
-  const lazyLoad = new IntersectionObserver((entries, lazyLoad) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        preloadImage(entry.target);
-        lazyLoad.unobserve(entry.target);
-      }
+    const lazyLoad = new IntersectionObserver((entries, lazyLoad) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          preloadImage(entry.target);
+          lazyLoad.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    images.forEach(image => {
+      lazyLoad.observe(image);
     });
-  }, options);
+  }
 
-  images.forEach(image => {
-    lazyLoad.observe(image);
-  });
+  function lateLoad() {
+    const images = $$.body.querySelectorAll('[data-lazy-late]');
+    if (!exists(images)) { return; }
+
+    images.forEach(image => {
+      image.src = image.dataset.lazyLate;
+    });
+  }
+
+  lazyLoad();
+  lateLoad();
 }();
 
 export default LazyLoad;
