3d397c30e506aa71ea78076b67c3fd6ef2961153
Matt Dwen <matt@dwen.co.nz>
2022-04-28 12:00:02 +1200

    Ensure contact is highlighted if scrolling to the end
    Fixes #63

--- resources/js/modules/scroll-spy/index.js
+++ resources/js/modules/scroll-spy/index.js
@@ -1,18 +1,28 @@
 import $$ from '@utilities/selectors';
 
+const contactIndex = 4;
+
 const checkPosition = () => {
   const fromTop = window.scrollY + 1;
 
   $$.navItems
-    .forEach(link => {
+    .forEach((link, index) => {
       if (link.hash === undefined || link.hash.length < 1) { return; }
 
       const section = document.querySelector(link.hash);
 
-      if (section.offsetTop <= fromTop && (section.offsetTop + section.offsetHeight) > fromTop) {
-        link.classList.add('active');
-      } else {
-        link.classList.remove('active');
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        if (index === contactIndex) {
+          link.classList.add('active');
+        } else {
+          link.classList.remove('active');
+        }
+      } else  {
+        if (section.offsetTop <= fromTop && (section.offsetTop + section.offsetHeight) > fromTop) {
+          link.classList.add('active');
+        } else {
+          link.classList.remove('active');
+        }
       }
     });
 }
